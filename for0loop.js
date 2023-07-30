d5d22002ad8dde8c2cf989872ad05f5f3c7cd71e
raselsharif <raselsharif9@gmail.com>
2023-07-30 22:20:21 +0600

    added for loop

---
+++ for0loop.js
@@ -0,0 +1,15 @@
+
+
+for(var i=0; i<=50;i++){
+    console.log(i);
+}
+
+// ===== even number ====
+for(var i=0; i<=50;i+=2){
+    console.log(i);
+}
+
+// ===== odd number ====
+for(var i=1; i<=50;i+=2){
+    console.log(i);
+}
