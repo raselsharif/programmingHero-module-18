8fc41a827d26f8261205068577f5e3aeae0799b8
raselsharif <raselsharif9@gmail.com>
2023-07-30 22:49:07 +0600

    added array with elements

---
+++ array-for-loop.js
@@ -0,0 +1,13 @@
+var numbers = [100, 200, 125, 142, 256, 2365, 896, 3265, 523, 654, 85];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
+
+var fruits = ["banana", "orange", "mango", "grape"];
+
+for (var i = 0; i < fruits.length; i++) {
+    var fruit = fruits[i];
+    console.log(fruit);
+}
