3e7ca8ae6b6b1cc38284fa6e3e97fc4fd4fc917d
raselsharif <raselsharif9@gmail.com>
2023-07-30 21:59:40 +0600

    added while loop number-odd-even

---
+++ numberWhileLoop.js
@@ -0,0 +1,23 @@
+var number = 1;
+
+while(number<=50){
+    console.log(number);
+    number++;
+}
+
+// ======= even number =====
+
+var number = 0;
+
+while(number<=50){
+    console.log(number);
+    number+=2;
+}
+// ======= odd number =====
+
+var number = 1;
+
+while(number<=50){
+    console.log(number);
+    number+=2;
+}
