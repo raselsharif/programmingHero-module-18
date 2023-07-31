0f7467b62f057b658397f144c36e0569cf083bb9
Rasel Sharif <raselsharif9@gmail.com>
2023-07-31 12:55:28 +0600

    added conditon practice

---
+++ condition.js
@@ -0,0 +1,14 @@
+
+var myAmount = 19500;
+
+if(myAmount>=80000){
+    console.log("Buy MACbook");
+}else if(myAmount>=60000){
+    console.log("buy a gaming laptop");
+}else if(myAmount>=40000){
+    console.log("Buy lenovo Yoga");
+}else if(myAmount>=20000){
+    console.log("Buy Used laptop");
+} else{
+    console.log("Manage more money for buy a laptop");
+}
